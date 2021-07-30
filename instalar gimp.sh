9dd50b8583b19a4b6260070655501c6ef68aa4cc
iiLH <80596529+iiLH@users.noreply.github.com>
2021-07-30 14:15:50 -0300

    lol

---
+++ instalar gimp.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo Ejecutable para instalar GIMP
+echo hecho por Lucca
+echo "escribe tu contraseña ahora! (si usas una ceibalita, la contraseña es "ceibal")"
+sudo apt-get install gimp
+echo Fin!
